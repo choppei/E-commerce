4f31a4efb3df84b37c67c6ce2366655b03cbf794
choppei <berk.kucukogluuwgmail.com>
2020-07-14 18:38:10 +0300

    succesfully

---
+++ VillaBerk/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VillaBerk.Models;
+
+namespace VillaBerk.Controllers
+{
+    public class HomeController : Controller
+    {
+        VillaBerkContext _context = new VillaBerkContext();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            var products = _context
+                .Products
+                .Where(i => i.isApproved == true && i.isHome == true)
+                .ToList();
+
+            return View(products);
+        }
+    }
+}
---
+++ VillaBerk/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VillaBerk.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public virtual List<Product> Products { get; set; }
+
+    }
+}
